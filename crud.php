059ba8496b0d49426309cd4c355134a3dcbd9f54
KevinGutej <76398856+KevinGutej@users.noreply.github.com>
2023-10-17 23:55:46 +0200

    Create

--- crud.php
+++ crud.php
@@ -1,5 +1,36 @@
 <?php
 
+
+$servername = "localhost";
+$username = "root";
+$password = "password123";
+$dbname = "CRUD";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Problem with connecting,  " . $conn->connect_error);
+}
+
+if (isset($_POST['create'])) {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+
+    $sql = "INSERT INTO users (name, email) VALUES ('$name', '$email')";
+
+    if ($conn->query($sql) === true) {
+        echo "Record created successfully.";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+}
+
+$conn->close();
+
+
+
+
+
 if (isset($_POST['read'])) {
     $sql = "SELECT * FROM users";
     $result = $connection->query($sql);
